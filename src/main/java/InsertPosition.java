0af0af07b7eac283fc9499a51270ab7db4090652
Winstonshih <Winstonshih@github.com>
2026-08-25 18:35:35 +0000

    Test run: 5/5 passed, 0 failed, 0 skipped - 2026-08-25T18:35:35.926Z

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -17,7 +17,7 @@ public class InsertPosition {
     public int searchInsert(int[] nums, int target) {
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]>=target)
+            if(nums[i]>target)
             {
                 return i;
             }
